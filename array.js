f9df0f5284bba3531b5b73b9bd598b532a41b0be
Antony Gla <94374096+nerd220@users.noreply.github.com>
2022-04-23 18:47:43 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,46 @@
+//		что работает?
+
+// создаём массив с помощью конструктора Arr
+// var a=new Arr('Яблоко','Банан','Апельсин'); 
+//
+// for(let v of a) 
+// перебираем массив, методом for of
+//
+// for(let i=0; i<a.length; i++) a[i] (а точнее a.array[i], т.к. array упорядочен по индексам от 0 до length-1)
+// перебираем массив методом for (при включённом proxy, возможно, замедляет работу)
+//
+// a['а']='Яблоко';
+// console.log(a['а'])
+// добавление элементов как в обычный массив (при включённом proxy, возможно, замедляет работу)
+//
+// a.push('Яблоко')
+// также работают unshift, shift, pop, splice (без третьего аргумента), reverse
+// особое внимание оператору sort, который работает, но его работа занимает дольше времени (т.к. требует переиндексацию)
+//
+// a.get(key), a.set(key,value), a.delete(key) и остальные методы Map
+//
+// a.array, a.map - доступ к данным в виде массива или в виде Map
+//
+// a.length, a.size
+// корректно работают операторы длины массива
+//
+// a.set('123','Банан') и a.set(123,'Банан') идентичны
+// они приведут к одному и тому же элементу, т.к. работает приведение типов ключей к числам и строкам (ключи-объекты запрещены)
+//
+// key in a
+// вернёт true или false в зависимости от наличия ключа в a
+//
+// ...a
+// spread оператор вернёт набор значений a
+
+//		что не работает
+
+// for(let i in a)
+//
+// delete a[key]
+
 //		для чего это нужно
- 
+
 // к сожалению, массивы, объекты и Map/Set в JS не реализуют стандартный необходимый функционал векторов (как, например, массивы в PHP)
 // например, невозможно сделать push в Map или Object
 // невозможно работать с нелинейными нечисловыми ключами в Array (при удалении/добавлении теряется length)
@@ -55,6 +96,19 @@
 //
 // ключи pushIndex%NUM% зарезервированы под внутренний индекс массива для корректного push-а
 
+var useProxy=1; // включить, чтобы иметь возможность доступа к массиву через a[key]
+var deepTrace=1;	// включить, чтобы исключить из доступа поля самого класса, например a['a'] и т.д. (работает только если включён useProxy)
+				// значительно замедляет работу
+
+// prepend
+var allowMethodsT=['array','map','foreach','copy','set','add','clear','delete','push','shift','pop','unshift','splice','reverse','sort','get','has','keys','values','entries','length','size','iter'];
+var allowMethodsArray=[];
+for(let key of allowMethodsT) allowMethodsArray[key]=1;
+delete allowMethodsT;
+var scriptsT = document.getElementsByTagName('script');
+var scriptNameArray = scriptsT[scriptsT.length-1].src.split('/');
+scriptNameArray=scriptNameArray[scriptNameArray.length-1];
+delete scriptsT;
 
 function Arr(){
 	this.def=function(){
@@ -188,40 +242,87 @@ function Arr(){
 	this.entries=function(){
 		return this.d.entries();
 	}
-	this.__defineGetter__("size", function(){
-		return this.d.size;
+	this.asProxy=function(){
+		let t=this;
+		let handler = {
+			get: function(target, prop){
+				let e=false;
+				if(deepTrace) e=new Error().stack.indexOf('Proxy.Arr')!==-1;
+				//if(deepTrace) e=new Error().stack.indexOf(scriptNameArray)!==-1;
+				if (typeof target[prop]!=='undefined' && (e || !deepTrace || (prop in allowMethodsArray))){
+					return target[prop];
+				} else {
+					if(t.d.has(t.prependKey(prop))){
+						return t.d.get(t.prependKey(prop));
+					}
+					else return false;
+				}
+			},
+			set: function(target, prop, value){
+				let e=false;
+				if(deepTrace) e=new Error().stack.indexOf('Proxy.Arr')!==-1;
+				if (typeof target[prop]!=='undefined' && (e || !deepTrace || (prop in allowMethodsArray))){
+					target[prop]=value;
+				} else {
+					t.set(prop,value);
+				}
+			}
+		}
+		return new Proxy(this, handler)
+	}
+	
+	let ok=Object.keys(this);
+	for(let i in ok) Object.defineProperty(this,ok[i],{enumerable:false});
+	Object.defineProperty(this,'length',{
+		get: function(){return this.d.size}
 	});
-	this.__defineGetter__("length", function(){
-		return this.d.size;
+	Object.defineProperty(this,'size',{
+		get: function(){return this.d.size}
 	});
-	this.__defineGetter__("length", function(){
-		return this.copy();
+	Object.defineProperty(this,'copy',{
+		get: function(){return this.copy()}
 	});
-	this.__defineGetter__("iter", function(){
-		let g=this.generator();
-		return g;
+	Object.defineProperty(this,'iter',{
+		get: function(){
+			let g=this.generator();
+			return g;
+		}
 	});
 	this[Symbol.iterator]=this.generator;
 	for(let i=0;i<arguments.length;i++){
 		this.push(arguments[i]);
 	}
+	if(useProxy) return this.asProxy();
 }
+
+// some tests & examples
 
 //var a=new Arr('Яблоко','Банан');
 //a.reverse();
 //a.unshift('Кирка');
 //a.sort();
 //for(let v of a) alert(v);
-
 //a.c.forEach((k,v)=>{alert(k+' '+v);});
 
 //var a=new Arr();
-//a.set('а','Яблоко');
+//a.set('a','Яблоко');
 //a.set('б','Банан');
+//a.set('123','Близнецы');
+//a.set(123,'Близнецы');
 //a.set('в','Сочи');
+//a['1234']='Красная поляна';
+//alert('а' in a);	//- true
+//alert('f' in a);	//- false
+//a['a'];
+//alert(a.length);
 //a.sort();
-
-
-//for(let v of a.iter){
-//	alert(v);
+//console.log(...a);
+//let s='';
+//for(let v of a){
+//	s+=v+', ';
 //}
+//a.sort();
+//for(let i=0; i<a.length; i++){
+//	s+=a.array[i];
+//}
+//console.log(s);
