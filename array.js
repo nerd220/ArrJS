7728d11889895035303f39ea637731b7d3ad78d7
Antony Gla <94374096+nerd220@users.noreply.github.com>
2022-04-26 16:35:06 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,107 +1,11 @@
-//		что работает?
-
-// создаём массив с помощью конструктора Arr
-// var a=new Arr('Яблоко','Банан','Апельсин'); 
-//
-// for(let v of a) 
-// перебираем массив, методом for of
-//
-// for(let i=0; i<a.length; i++) a[i] (а точнее a.array[i], т.к. array упорядочен по индексам от 0 до length-1)
-// перебираем массив методом for (при включённом proxy, возможно, замедляет работу)
-//
-// a['а']='Яблоко';
-// console.log(a['а'])
-// добавление элементов как в обычный массив (при включённом proxy, возможно, замедляет работу)
-//
-// a.push('Яблоко')
-// также работают unshift, shift, pop, splice (без третьего аргумента), reverse
-// особое внимание оператору sort, который работает, но его работа занимает дольше времени (т.к. требует переиндексацию)
-//
-// a.get(key), a.set(key,value), a.delete(key) и остальные методы Map
-//
-// a.array, a.map - доступ к данным в виде массива или в виде Map
-//
-// a.length, a.size
-// корректно работают операторы длины массива
-//
-// a.set('123','Банан') и a.set(123,'Банан') идентичны
-// они приведут к одному и тому же элементу, т.к. работает приведение типов ключей к числам и строкам (ключи-объекты запрещены)
-//
-// ...a
-// spread оператор вернёт набор значений a
-
-//		что не работает
-
-// for(let i in a)
-//
-// delete a[key]
-//
-// key in a
-
-//		для чего это нужно
-
-// к сожалению, массивы, объекты и Map/Set в JS не реализуют стандартный необходимый функционал векторов (как, например, массивы в PHP)
-// например, невозможно сделать push в Map или Object
-// невозможно работать с нелинейными нечисловыми ключами в Array (при удалении/добавлении теряется length)
-// пример: x[25]=1 вернёт length, равной 26 даже если это единственный элемент массива
-// если мы после этого сделаем x['test']=2, то length всё равно будет равен 26
-// по этому было решено объеденить функционал Map и Array
-// плюсы:
-// - вам доступен функционал Map
-// - вам доступен функционал Array
-// минусы:
-// - медленней скорость работы
-// - данные занимают больше памяти
-
-//		расширенный функционал
-
-//a.array	- работа так, как если бы это был массив (это и есть массив)
-//a.map	- работа так, как если бы это был объект Map (это и есть объект Map)
-//a.copy	- работа так, как если бы это был массив (работать с копией массива)
-//a.iter	- итератор для перебора для for of
-//a.foreach( f(key,value) )	- перебор значений, если вы хотите завершить перебор, верните false
-//a.push()	- добавить элемент v в массив
-//a.shift()
-//a.pop()
-//a.unshift()
-//a.splice(pos,deleteCount)		- обратите внимание, что здесь нет третьего элемента items
-//a.reverse
-//a.sort
-//также можно использовать функции массива через a.array и функции Map через a.map
-
-//		стандартный функционал (с доработками, вроде коррекции ключа и синхронизации с a,b,c)
-
-//a.has	- проверяет, есть ли элемент с таким ключом
-//a.size	- размер массива
-//a.length	- ~
-//a.clear	- очистить массив
-//a.set	- установить значение
-//a.get	- получить значение
-//a.delete	- удалить значение с этим ключом
-//a.keys
-//a.values
-//a.entries
-
-//		как это устроено
-
-// a.a - содержит массив ключей
-// a.b - содержит массив значений
-// a.c - содержит Map ключ -> индекс массивов a и b
-// a.d - содержит Map ключ -> значение
-
-//		внимание
-
-// допускаются только текстовые и числовые ключи
-//
-// ключи pushIndex%NUM% зарезервированы под внутренний индекс массива для корректного push-а
-
-var useProxy=0; // включить, чтобы иметь возможность доступа к массиву через a[key]
-var deepTrace=0;	// включить, чтобы исключить из доступа поля самого класса, например a['a'] и т.д. (работает только если включён useProxy)
-				// значительно замедляет работу
-var useMap=0; // использовать отдельный Map с ключами-значениями
+// vars
+var useProxy=1; // enable, to access a[key]
+var deepTrace=1; // enable, to ignore inner class fields (liks 'a', 'b' and etc)
+				// very slow, because it's Error.log parsing
+var useMap=1; // use alter Map storage on a.map field
 
 // prepend
-var allowMethodsT=['array','map','foreach','copy','set','add','clear','delete','push','shift','pop','unshift','splice','reverse','sort','ksort','get','has','keys','values','entries','length','size','iter'];
+var allowMethodsT=['array','map','foreach','copy','set','add','clear','delete','push','shift','pop','unshift','splice','reverse','sort','ksort','get','has','keys','values','entries','length','size','iter','iterE'];
 var allowMethodsArray=[];
 for(let key of allowMethodsT) allowMethodsArray[key]=1;
 delete allowMethodsT;
@@ -123,6 +27,9 @@ function Arr(){
 	this.def();
 	this.generator=function*(){
 		for(let i=0;i<this.a.length;i++) yield this.b[i];
+	}
+	this.generatorE=function*(){
+		for(let i=0;i<this.a.length;i++) yield [this.a[i],this.b[i]];
 	}
 	this.foreach=function(f){
 		for(let i=0;i<this.a.length;i++){
@@ -327,39 +234,15 @@ function Arr(){
 			return g;
 		}
 	});
+	Object.defineProperty(this,'iterE',{
+		get: function(){
+			let g=this.generatorE();
+			return g;
+		}
+	});
 	this[Symbol.iterator]=this.generator;
 	for(let i=0;i<arguments.length;i++){
 		this.push(arguments[i]);
 	}
 	if(useProxy) return this.asProxy();
 }
-
-// some tests & examples
-
-//var a=new Arr('Яблоко','Банан');
-//a.reverse();
-//a.unshift('Кирка');
-//a.sort();
-//for(let v of a) alert(v);
-//a.c.forEach((k,v)=>{alert(k+' '+v);});
-
-//var a=new Arr();
-//a.set('а','Яблоко');
-//a.set('б','Банан');
-//a.set('123','Близнецы');
-//a.set(123,'Близнецы');
-//a.set('в','Сочи');
-//a['1234']='Красная поляна';
-//a['a'];
-//alert(a.length);
-//a.sort();
-//console.log(...a);
-//let s='';
-//for(let v of a){
-//	s+=v+', ';
-//}
-//a.sort();
-//for(let i=0; i<a.length; i++){
-//	s+=a.array[i];
-//}
-//console.log(s);
